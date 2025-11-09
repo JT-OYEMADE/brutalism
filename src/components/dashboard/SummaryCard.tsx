bad4b36e8ca15e1451153cbf0bb68a89490bee8d
Tomisin <oyemadeoluwatomisin75@gmail.com>
2025-11-10 00:47:09 +0100

    ui changes

--- src/components/dashboard/SummaryCard.tsx
+++ src/components/dashboard/SummaryCard.tsx
@@ -15,17 +15,17 @@ export const SummaryCard = ({
 }: SummaryCardProps) => {
   return (
     <div className='border border-[#787878] p-4 bg-white'>
-      <h4>{title}</h4>
+      <h4 className='font-medium'>{title}</h4>
       <div className='flex justify-between mt-2 mb-5'>
         <div>
           <div className='text-2xl font-bold'>{balance}</div>
           <p className='text-sm'>Previous</p>
-          <div className='text-xs'>{previousBal}</div>
+          <div className='text-sm font-semibold '>{previousBal}</div>
         </div>
         <div className='text-right flex flex-col items-end'>
           {icon}
           <p className='text-sm'>Progress</p>
-          <div className='text-xs text-[#7DD03E]'>{progress}%</div>
+          <div className='text-sm text-[#7DD03E] font-semibold'>{progress}%</div>
         </div>
       </div>
     </div>
